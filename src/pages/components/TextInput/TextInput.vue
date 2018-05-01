c3c78deff6d1269b95f6c4e4dda98de20057bbe0
Perry <brothergilburt@gmail.com>
2018-04-30 20:01:20 -0500

    Created new TextInput component.

---
+++ src/pages/components/TextInput/TextInput.vue
@@ -0,0 +1,43 @@
+<template>
+<div class="gw2-profile-input-container">
+  <input class="gw2profile-input" type="text" :placeholder="placeholder" :value="value" @input="update($event.target.value)" :maxLength="max"/>
+  <span v-if="errorMessage" class="gw2profile-input-message">{{errorMessage}}</span>
+</div>
+</template>
+
+<script>
+export default {
+  props: {
+    errorMessage: {
+      type: String,
+      default: ''
+    },
+    max: {
+      type: Number,
+      default: 524288
+    },
+    placeholder: {
+      type: String,
+      default: ''
+    },
+    value: {
+      type: String,
+      required: true
+    }
+  },
+  methods: {
+    update(value) {
+      this.$emit('input', value)
+    }
+  },
+  computed: {
+    isError() {
+      return this.errorMessage !== '';
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
